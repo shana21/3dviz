202e61cf0d67021c4a16a278bc0962d4e6e0cbb5
Scrim <bgtm1260133@gn.iwasaki.ac.jp>
2017-12-20 10:32:44 +0900

    カメラの調整、一部Assetのインポート

---
+++ Assets/Data.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+
+    public static int intValue;
+    
+	// Use this for initialization
+	void Start () {
+        Debug.Log("" + intValue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public int returnInt()
+    {
+        return intValue;
+    }
+}
---
+++ Assets/locatetest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class locatetest : MonoBehaviour {
+    GameObject obj;
+    public double longitude=35.285160+0f, latitude=139.674364+0f;
+
+    private Vector3 direction;
+    private Vector3 moveDirection = new Vector3();
+
+    // Use this for initialization
+    void Start () {
+        obj = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float veclat = (float)(latitude - 139)*2000;
+        float veclong = (float)(longitude - 35)*2000;
+
+        
+        transform.position=Vector3.MoveTowards(new Vector3(0,0,0),new Vector3(veclat,0, veclong),5000f);
+        //obj.transform.position = vec;
+    }
+}
